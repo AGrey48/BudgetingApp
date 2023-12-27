70224d8e38f58aa8a5e8094f665f8e4c8e29aa55
AGrey48 <nightskies135@gmail.com>
2023-12-27 09:21:00 -0700

    Added more code to work on showing results in the same webpage instead of a new page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -7,12 +7,39 @@ app = Flask(__name__)
 def home():  # put application's code here
     return render_template('home.html')
 
-@app.route('/submit_budget', methods = ['POST'])
+
+@app.route('/process_data', methods=['POST'])
+def process_data():
+    """
+    Receive data from AJAX request.
+    Allows for website to update parts of it without having to reload the entire webpage
+    :return:jsonify
+    """
+    data = request.get_json()  # dictionary, JSON results come back as a dictionary
+    income = data['income']
+    expense = data['expense']
+    result = income - expense
+
+    # jsonify converts the python dictionary into a JSON object.
+    # The JSON object can be sent as a response to an HTTP request.
+    return jsonify({'result': 'test'})
+
+
+@app.route('/submit_budget', methods=['POST'])
 def submit_budget():
-    income = request.form['income']
-    expense = request.form['expense']
-    #process budget data here
-    return render_template('results.html', income=income, expense=expense)
+    try:
+        print("request received:", request.form)
+        income = request.form['income']
+        expense = request.form['expense']
+        print("Income:", income, "Expense", expense)
+
+        print("Received data:", request.form)
+        balance = float(income) - float(expense)
+        print("Sending data:", balance)
+        return jsonify({'result': 'Success'})
+    except Exception as e:
+        print("Error: ", e)
+        return jsonify({'error': str(e)}), 400
 
 
 if __name__ == '__main__':
--- static/script.js
+++ static/script.js
@@ -1,4 +1,21 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('date-time').innerText = new Date().toLocaleTimeString();
-})
 
+    document.getElementById('budgetForm').addEventListener('submit', function(e) {
+        e.preventDefault() //prevent default form submission
+
+        var formData = new FormData(document.getElementById('budgetForm')); //grabs the data from your form
+
+        fetch('/submit_budget', {
+            method: 'POST',
+            body: formData
+            })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Response data:", data);
+            // Additional code to update your page with the response data
+            document.getElementById('resultDisplay').innerText = 'Calculated Balance: ' + data.result;
+        })
+        .catch(error => console.error('Error', error));
+        });
+    });
