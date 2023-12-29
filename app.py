468a91a29a1fa3012c064a0fe992642151ffd62a
AGrey48 <nightskies135@gmail.com>
2023-12-29 09:14:14 -0700

    Added tabs to app. WIP

--- app.py
+++ app.py
@@ -7,24 +7,6 @@ app = Flask(__name__)
 def home():  # put application's code here
     return render_template('home.html')
 
-
-@app.route('/process_data', methods=['POST'])
-def process_data():
-    """
-    Receive data from AJAX request.
-    Allows for website to update parts of it without having to reload the entire webpage
-    :return:jsonify
-    """
-    data = request.get_json()  # dictionary, JSON results come back as a dictionary
-    income = data['income']
-    expense = data['expense']
-    result = income - expense
-
-    # jsonify converts the python dictionary into a JSON object.
-    # The JSON object can be sent as a response to an HTTP request.
-    return jsonify({'result': 'test'})
-
-
 @app.route('/submit_budget', methods=['POST'])
 def submit_budget():
     try:
@@ -36,7 +18,7 @@ def submit_budget():
         print("Received data:", request.form)
         balance = float(income) - float(expense)
         print("Sending data:", balance)
-        return jsonify({'result': 'Success'})
+        return jsonify({'result': balance})
     except Exception as e:
         print("Error: ", e)
         return jsonify({'error': str(e)}), 400
--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
+//display time
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('date-time').innerText = new Date().toLocaleTimeString();
-
+// update data without making a new web page
     document.getElementById('budgetForm').addEventListener('submit', function(e) {
         e.preventDefault() //prevent default form submission
 
@@ -19,3 +20,44 @@ document.addEventListener('DOMContentLoaded', (event) => {
         .catch(error => console.error('Error', error));
         });
     });
+
+function budgetFunct(evt, currentTab){
+    var i, tabContent, tabLinks;
+    console.log("function is running")
+
+    // Get all elements with class="tabContent" and hide them
+    tabContent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabContent.length; i++){
+        tabContent[i].className = tabContent[i].className.replace("none");
+    }
+
+    // Get all elements with class="tabLinks" and remove class "active"
+    tabLinks = document.getElementsByClassName("tabLinks");
+    for(i = 0; i < tabLinks.length; i++){
+        tabLinks[i].className = tabLinks.className.replace(" active", "");
+    }
+
+    document.getElementById(currentTab).style.display = "block";
+    evt.currentTarget.className += " active";
+    document.getElementById("defaultOpen").click();
+}
+
+document.getElementById('budgetForm').addEventListener('submit', function(e) {
+    e.preventDefault(); <!--Prevents default form submission-->
+
+    var formData = new FormData(this); <!-- Grabs the data from your form -->
+
+    <!-- AJAX request to your Flask route -->
+    fetch('/submit_budget', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.json()) // assuming the response is JSON>
+.
+    then(data => {
+        <!-- Display the result -->
+        document.getElementById('result').innerHTML = 'Result' + data.result;
+
+    })
+        .catch(error => console.error('Error', error));
+});
